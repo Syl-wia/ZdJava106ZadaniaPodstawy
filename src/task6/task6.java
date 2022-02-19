fe07bbaa899020d3dc8d644069732617b88b614f
Syl-wia <sylwiakolek6@gmail.com>
2022-02-19 14:56:01 +0100

    task6

--- src/task6/task6.java
+++ src/task6/task6.java
@@ -1,6 +1,24 @@
 package task6;
 
 public class task6 {
+    public static void main(String[] args) {
+        printMultiplicationTable(6,2,5);
 
+    }
+
+    private static void printMultiplicationTable(int multiplier, int minMuliplicand, int maxMultiplicand){
+        // for (;;)
+  //      for(; minMuliplicand <= maxMultiplicand; minMuliplicand++) {
+  //          int result = multiplier * minMuliplicand;
+  //          System.out.println(multiplier + " * " + minMuliplicand + " = " + result);
+  //      }
+
+        while(minMuliplicand <= maxMultiplicand){
+            int result = multiplier * minMuliplicand; // 6* 2 = 12
+            System.out.println(multiplier + " * " + minMuliplicand + " = " + result);
+            minMuliplicand++; // 3
+
+            }
+        }
 
 }
